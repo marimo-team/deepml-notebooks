4db350167fe4a922ce5dc02678f031e2c0a56dab
Akshay Agrawal <akshaykagrawal7@gmail.com>
2025-01-04 17:01:43 -0800

    edits: k-means notebook
    * simplify notebook by removing iris dataset
    * fix generation of random data -- clicking the run button was
      regenerating data on each point
    * generate more interesting data on random so that the learner has to
      intelligently choose the number of clusters (there is roughly a right
    answer for each randomly generated dataset)
    * left more TODOs

--- problems/problem-17/notebook.py
+++ problems/problem-17/notebook.py
@@ -1,3 +1,15 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "drawdata==0.3.6",
+#     "marimo",
+#     "numpy==2.2.1",
+#     "pandas==2.2.3",
+#     "plotly==5.24.1",
+#     "scikit-learn==1.6.0",
+# ]
+# ///
+
 import marimo
 
 __generated_with = "0.10.9"
@@ -5,19 +17,21 @@ app = marimo.App()
 
 
 @app.cell
-def _(mo):
-    demo_type = mo.ui.dropdown(
-        options=["K-means Interactive Demo", "Iris Dataset Clustering"],
-        value="K-means Interactive Demo",
-        label="Select Demo"
-    )
-    return (demo_type,)
+def _():
+    import random
+
+    import numpy as np
+    import pandas as pd
+    from sklearn.cluster import KMeans
+    import plotly.express as px
+    from drawdata import ScatterWidget
+    return KMeans, ScatterWidget, np, pd, px, random
 
 
 @app.cell
-def _(demo_type):
-    demo_type
-    return
+def _():
+    import marimo as mo
+    return (mo,)
 
 
 @app.cell
@@ -31,8 +45,8 @@ def _(mo):
 
 
 @app.cell
-def _():
-    # method
+def _(method):
+    method
     return
 
 
@@ -43,45 +57,113 @@ def _(mo):
 
 
 @app.cell
-def _():
-    # points
-    return
-
-
-@app.cell
 def _(mo):
     k_clusters = mo.ui.number(value=5, start=2, stop=10, label="Number of Clusters")
+    k_clusters
     return (k_clusters,)
 
 
 @app.cell
-def _():
-    # k_clusters
-    return
-
-
-@app.cell
 def _(mo):
-    run_button = mo.ui.run_button(label="Run")
-    return (run_button,)
-
-
-@app.cell
-def _():
-    # run_button
-    return
-
-
-@app.cell
-def _(mo):
-    random_button = mo.ui.button(label="Random")
+    random_button = mo.ui.button(label="Generate new data")
     return (random_button,)
 
 
 @app.cell
-def _():
-    # random_button
+def _(run_button):
+    run_button
     return
+
+
+@app.cell
+def _(method, mo, random_button, widget):
+    random_button if method.value == "Random" else mo.md(
+        f"""
+        Draw a dataset of points, then click the run button above!
+
+        {widget}
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    run_button = mo.ui.run_button(label="Run k-means!")
+    return (run_button,)
+
+
+@app.cell
+def _(np, random, random_button):
+    random_button
+
+
+    def _generate_data():
+        n_clusters = random.randint(2, 10)
+        np.random.randn()
+
+        points = []
+        for i in range(n_clusters):
+            points.append(
+                np.random.randn(100, 2) * np.random.uniform(-2, 2)
+                + np.random.uniform(-2, 2)
+            )
+        return np.vstack(points)
+
+
+    generated_points = _generate_data()
+    return (generated_points,)
+
+
+@app.cell
+def _(
+    KMeans,
+    generated_points,
+    k_clusters,
+    method,
+    np,
+    pd,
+    px,
+    run_button,
+    widget,
+):
+    fig = px.scatter(
+        x=generated_points[:, 0], y=generated_points[:, 1], title="Random Points"
+    ) if method.value == "Random" else None
+
+    if run_button.value and method.value == "Random":
+        kmeans = KMeans(n_clusters=k_clusters.value, random_state=42)
+        clusters = kmeans.fit_predict(generated_points)
+        df = pd.DataFrame(generated_points, columns=["x", "y"])
+        df["cluster"] = clusters
+
+        fig = px.scatter(
+            df,
+            x="x",
+            y="y",
+            color="cluster",
+            title="K-means Clustering (Random Data)",
+            color_continuous_scale="viridis",
+        )
+    elif run_button.value and method.value == "Manual":
+        df = widget.data_as_pandas
+        if not df.empty:
+            # Drop non-numeric columns (like 'colour')
+            numeric_df = df.select_dtypes(include=[np.number])
+            kmeans = KMeans(n_clusters=k_clusters.value, random_state=42)
+            clusters = kmeans.fit_predict(numeric_df)
+            df['cluster'] = clusters
+            fig = px.scatter(
+                df,
+                x='x',
+                y='y',
+                color='cluster',
+                title="K-means Clustering (Manual Data)",
+                color_continuous_scale='viridis'
+            )
+
+    fig
+    return clusters, df, fig, kmeans, numeric_df
 
 
 @app.cell
@@ -91,187 +173,18 @@ def _(ScatterWidget, mo):
 
 
 @app.cell
-def _():
-    # _df = widget.data_as_pandas
-    # _df
-    return
+def _(mo):
+    mo.md(
+        """
+        TODO:
 
-
-@app.cell
-def _():
-    # widget.value
-    return
-
-
-@app.cell
-def _(
-    KMeans,
-    datasets,
-    demo_type,
-    k_clusters,
-    method,
-    mo,
-    np,
-    pd,
-    points,
-    px,
-    random_button,
-    run_button,
-    widget,
-):
-    if demo_type.value == "K-means Interactive Demo":
-        # if method.value == "Manual":
-        #     # widget = mo.ui.anywidget(ScatterWidget())
-        #     if run_button.value:
-        #         df = widget.data_as_pandas
-        #         if not df.empty:
-        #             kmeans = KMeans(n_clusters=k_clusters.value, random_state=42)
-        #             clusters = kmeans.fit_predict(df)
-        #             df['cluster'] = clusters
-        #             fig = px.scatter(
-        #                 df,
-        #                 x='x',
-        #                 y='y',
-        #                 color='cluster',
-        #                 title="K-means Clustering (Manual Data)",
-        #                 color_continuous_scale='viridis'
-        #             )
-        #             display = mo.ui.plotly(fig)
-        #         else:
-        #             display = widget
-        #     else:
-        #         display = widget
-        if method.value == "Manual":
-            # widget = mo.ui.anywidget(ScatterWidget())
-            if run_button.value:
-                df = widget.data_as_pandas
-                if not df.empty:
-                    # Drop non-numeric columns (like 'colour')
-                    numeric_df = df.select_dtypes(include=[np.number])
-                    kmeans = KMeans(n_clusters=k_clusters.value, random_state=42)
-                    clusters = kmeans.fit_predict(numeric_df)
-                    df['cluster'] = clusters
-                    fig = px.scatter(
-                        df,
-                        x='x',
-                        y='y',
-                        color='cluster',
-                        title="K-means Clustering (Manual Data)",
-                        color_continuous_scale='viridis'
-                    )
-                    display = mo.ui.plotly(fig)
-                else:
-                    display = widget
-            else:
-                display = widget
-        else:
-            generated_points = np.random.rand(points.value, 2) * 500
-            if run_button.value:
-                kmeans = KMeans(n_clusters=k_clusters.value, random_state=42)
-                clusters = kmeans.fit_predict(generated_points)
-                df = pd.DataFrame(generated_points, columns=['x', 'y'])
-                df['cluster'] = clusters
-
-                fig = px.scatter(
-                    df,
-                    x='x',
-                    y='y',
-                    color='cluster',
-                    title="K-means Clustering (Random Data)",
-                    color_continuous_scale='viridis'
-                )
-            else:
-                fig = px.scatter(
-                    x=generated_points[:, 0],
-                    y=generated_points[:, 1],
-                    title="Random Points"
-                )
-            display = mo.ui.plotly(fig)
-
-        mo.hstack([
-            mo.vstack([method, points, k_clusters, run_button, random_button]),
-            display
-        ])
-
-    elif demo_type.value == "Iris Dataset Clustering":
-        iris = datasets.load_iris()
-        data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-        kmeans = KMeans(n_clusters=k_clusters.value, random_state=42)
-        data['cluster'] = kmeans.fit_predict(data)
-
-        fig = px.scatter_3d(
-            data,
-            x='sepal length (cm)',
-            y='sepal width (cm)',
-            z='petal length (cm)',
-            color='cluster',
-            title='K-Means Clustering on Iris Dataset'
-        )
-
-        mo.vstack([k_clusters, mo.ui.plotly(fig)])
-    return (
-        clusters,
-        data,
-        df,
-        display,
-        fig,
-        generated_points,
-        iris,
-        kmeans,
-        numeric_df,
+        * Explanatory text about what k-means is
+        * What are the centroids?
+        * How many iterations does the algorithm run for?
+        * What is the loss of the algorithm at each step?
+        """
     )
-
-
-@app.cell
-def _():
-    # [mo.hstack([
-    #         mo.vstack([method, points, k_clusters, run_button, random_button]),
-    #         display
-    # ]) if demo_type.value == "K-means Interactive Demo" else mo.vstack([k_clusters, mo.ui.plotly(fig)])]
     return
-
-
-@app.cell
-def _(
-    demo_type,
-    display,
-    fig,
-    k_clusters,
-    method,
-    mo,
-    points,
-    random_button,
-    run_button,
-):
-    mo.hstack([
-        (mo.vstack([method, points, k_clusters, run_button, random_button]),
-        display) if demo_type.value == "K-means Interactive Demo" and method.value == "Random"
-        else
-        (mo.vstack([method, k_clusters, run_button]),
-        display) if demo_type.value == "K-means Interactive Demo" and method.value == "Manual"
-        else
-        (mo.vstack([k_clusters, mo.ui.plotly(fig)]))
-    ])
-    return
-
-
-@app.cell
-def _():
-    # fig
-    # display
-    return
-
-
-@app.cell
-def _():
-    import marimo as mo
-    import numpy as np
-    import pandas as pd
-    from sklearn.cluster import KMeans
-    from sklearn import datasets
-    import plotly.express as px
-    from drawdata import ScatterWidget
-    return KMeans, ScatterWidget, datasets, mo, np, pd, px
 
 
 if __name__ == "__main__":
